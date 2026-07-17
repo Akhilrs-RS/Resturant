aed630f6bcdf9934601dcc002119d90faf62fc5e
Akhil R S <akhilrajanshiners@gmail.com>
2026-07-17 23:42:30 +0530

    ok

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -8,6 +8,17 @@ export default {
     extend: {
       fontFamily: {
         sans: ['"Plus Jakarta Sans"', '"Outfit"', 'sans-serif'],
+        serif: ['"Playfair Display"', '"Cinzel"', 'serif'],
+        cinzel: ['"Cinzel"', 'serif'],
+      },
+      colors: {
+        resort: {
+          gold: '#c49b55',
+          'gold-hover': '#b89047',
+          'gold-light': '#dfbe82',
+          dark: '#0a0d14',
+          navy: '#111622',
+        }
       },
       backdropBlur: {
         xs: '2px',
